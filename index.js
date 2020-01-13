d94d5ae984c01459ccbf9774e6c93e23f41ffa55
Quentin <quentin@adelios.fr>
2020-01-13 17:06:15 +0100

    changed paths to mixin

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var path = require('path');
 var package = require('./package.json');
 
 var entryPoint = require.resolve(package.name);
-var distPath = path.join(path.dirname(entryPoint), 'font-face.scss');
+var distPath = path.join(path.dirname(entryPoint), 'scss');
 
 exports.includePath = distPath;
