7ff0019a7414cc330bad44da6f6181f1b4df68bd
Quentin <quentin@adelios.fr>
2020-01-13 16:59:38 +0100

    added the index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var path = require('path');
+var package = require('./package.json');
+
+var entryPoint = require.resolve(package.name);
+var distPath = path.join(path.dirname(entryPoint), 'dist');
+
+exports.includePath = distPath;
