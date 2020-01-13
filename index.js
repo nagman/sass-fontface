0f174255915ac75e8ac2b52ad7b47e101da5ee3d
Quentin <quentin@adelios.fr>
2020-01-13 17:13:44 +0100

    deleted index (useless)

--- index.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-var package = require('./package.json');
-
-var entryPoint = require.resolve(package.name);
-var distPath = path.join(path.dirname(entryPoint), 'scss');
-
-exports.includePath = distPath;
